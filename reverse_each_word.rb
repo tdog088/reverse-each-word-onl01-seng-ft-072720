43adf2e1b144e1c779bdf46cf5a24f905838477e
Taliyah Wilson <taliyahwilson27@gmail.com>
2020-08-06 04:22:13 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,9 +1,8 @@
-reverse_each_word(string)
+def reverse_each_word(string)
   array = string.split(" ")
   new_arr = []
   array.collect do |word|
     new_arr.push(word.reverse)
   end
   new_arr.join(" ")
-
 end
